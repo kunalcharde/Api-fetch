5aff1cb99a9912e199f3407689fe1a21a486e7ab
Kunal Charde <kunalcharde1@gmail.com>
2022-11-26 20:04:41 +0530

    files added

--- script.js
+++ script.js
@@ -5,14 +5,32 @@ fetch(
 }).then((objectdata)=>{
     let tabledata="";
     objectdata.map((values)=>{
+
+      // this function will change the number to currency
+      let number1 = values.current_price;
+        let formatted1 = new Intl.NumberFormat("en-US",{
+          style: 'currency',
+          currency:'USD'
+        }).format(number1);
+      let number2 = values.market_cap_change_24h;
+        let formatted2 = new Intl.NumberFormat("en-US",{
+          style: 'currency',
+          currency:'USD'
+        }).format(number2);
+      let number3 = values.market_cap;
+        let formatted3 = new Intl.NumberFormat("en-US",{
+          style: 'currency',
+          currency:'USD'
+        }).format(number3);
+        // ---------
         tabledata +=`<tr style="Border-bottom:2px solid white">
         <td><img src="${values.image}">  ${values.name}</td>
         <td>${(values.symbol).toUpperCase()}</td>
-        <td>$${values.current_price}</td>
-        <td>$${(values.market_cap_change_24h)}</td>
-        <td class="parcent">${parseFloat(values.market_cap_change_percentage_24h).toFixed(2)}%</td>
-        <td>Mkt Cap: ${values.market_cap}</td>
-      </tr>`;
+        <td>${formatted1}</td>
+        <td>${formatted2}</td>
+        <td>${parseFloat(values.market_cap_change_percentage_24h).toFixed(2)}%</td>
+        <td>Mkt Cap: ${formatted3}</td> 
+      </tr>`; //values.market_cap
     })
     document.getElementById("table_body").innerHTML=tabledata;
 
